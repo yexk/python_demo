df879318767560c1cc807c6ecdacac4271ebc51a
Yexk <yexk@yexk.cn>
2020-10-13 19:58:08 +0800

    [add] test

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 进入容器安装
 
-```
+``` sh
 pip install -r requirements.txt
 ```
 
-> TODO 后续解决自动安装的问题 
+> TODO 后续解决自动安装的问题
 
+``` sh
+python test.py
 ```
-python test.py 
-```
+
 > 获取百度的top数据
--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class GetBaidu:
     def __init__(self):
         self.redisdb = redis.Redis(host='redis', port=6379, db=0)
 
-    def getTopContents(self,key):
+    def getTopContents(self, key):
         urls = 'https://www.baidu.com'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'}
@@ -21,17 +21,18 @@ class GetBaidu:
                 "#hotsearch-content-wrapper > li > a > span.title-content-title")
         for i in data:
             t = i.contents[0]
-            self.redisdb.lpush(rediskey, t)
-            # print(__name__ ,"获取的内容:", t)
+            self.redisdb.rpush(rediskey, t)
 
+            # print(__name__ ,"获取的内容:", t)
     def getTopLists(self, key):
         return self.redisdb.lrange(key, 0, -1)
 
+
 if __name__ == "__main__":
-    times = str(time.strftime("%y%d%m%H%M%S"))
+    times = str(time.strftime("%Y%m%d%H%M%S"))
     rediskey = 'top:'+times
     baidu = GetBaidu()
-    baidu.getTopContents(rediskey) # 获取百度的top值
-    lists = baidu.getTopLists(rediskey) # 获取redis的缓存
+    baidu.getTopContents(rediskey)  # 获取百度的top值
+    lists = baidu.getTopLists(rediskey)  # 获取redis的缓存
     for i in lists:
         print(i.decode('utf-8'))
